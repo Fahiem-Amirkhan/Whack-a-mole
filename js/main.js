0b5a099844e3effb07786270e0f25ab3409a0590
Fahiem-Amirkhan <302514756@student.rocmondriaan.nl>
2024-06-03 14:48:00 +0200

    d

--- js/main.js
+++
@@ -1,7 +0,0 @@
-console.log('Main loaded');
-
-//functie voor als je de home pagina opent.
-let firstName;
-
-firstName = prompt('What is you name?');
-alert("Welcome to our gamepage " + firstName + "!\n\nOn this page you gonna see 3 games that we've build. We've made 3 diffrent games, Tic-Tac-Toe, Whack-a-Mole and Zeeslag. These three games are very diffrent from each other and it can get a little difficult.\n\n So if your ready, pick a game and enjoy!");
